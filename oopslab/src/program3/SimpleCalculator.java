9b664107d190185219079f587e2724a911cf86f0
vinayak-thakur <122772765+vinayak-thakur@users.noreply.github.com>
2023-04-07 20:26:15 +0530

    Update SimpleCalculator.java

--- oopslab/src/program3/SimpleCalculator.java
+++ oopslab/src/program3/SimpleCalculator.java
@@ -8,14 +8,14 @@ class SimpleCalculator {
 		char operator;
 		Double number1, number2, result;
 
-		// create an object of Scanner class
+		
 		Scanner input = new Scanner(System.in);
 
-		// ask users to enter operator
+		
 		System.out.println("Choose an operator: +, -, *, or /");
 		operator = input.next().charAt(0);
 
-		// ask users to enter numbers
+		
 		System.out.println("Enter first number");
 		number1 = input.nextDouble();
 
@@ -24,25 +24,25 @@ class SimpleCalculator {
 
 		switch (operator) {
 
-			// performs addition between numbers
+			
 			case '+':
 				result = number1 + number2;
 				System.out.println(number1 + " + " + number2 + " = " + result);
 				break;
 
-			// performs subtraction between numbers
+			
 			case '-':
 				result = number1 - number2;
 				System.out.println(number1 + " - " + number2 + " = " + result);
 				break;
 
-			// performs multiplication between numbers
+			
 			case '*':
 				result = number1 * number2;
 				System.out.println(number1 + " * " + number2 + " = " + result);
 				break;
 
-			// performs division between numbers
+			
 			case '/':
 				result = number1 / number2;
 				System.out.println(number1 + " / " + number2 + " = " + result);
