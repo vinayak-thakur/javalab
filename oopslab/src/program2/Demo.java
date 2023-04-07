1c47fc5223bf859a7fce17e490f693ce2fdc3132
vinayak- thakur <vinayak.cmrit@gmail.com>
2023-04-08 01:22:08 +0530

    commit

--- oopslab/src/program2/Demo.java
+++ oopslab/src/program2/Demo.java
@@ -5,27 +5,27 @@ public class Demo {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n;
-		System.out.println("Enter the number of students:");
-		n =sc.nextInt();
-		Student s[]=new Student[n];
-		for(int i=0;i<n;i++) {
-			s[i]=new Student();
-			System.out.println("Enter the USN of"+ (i+1) +"student");
+		System.out.println("Enter the number of students: ");
+		n = sc.nextInt();
+		Student s[] = new Student[n];
+		for(int i = 0; i < n ; i++) {
+			s[i] = new Student();
+		}
+		for(int i = 0; i < n ; i++) {
+			System.out.println("Enter the details of Student "+(i+1));
+			System.out.println("Enter the USN\n");
 			s[i].usn = sc.next();
-			System.out.println("Enter the name of"+ (i+1) +"student");
+			System.out.println("Enter the Name\n");
 			s[i].name = sc.next();
-			System.out.println("Enter the branch of"+ (i+1) +"student");
-			s[i].branch= sc.next();
-			System.out.println("Enter the phone number of"+ (i+1) +"student");
-			s[i].phoneno = sc.nextLong();
+			System.out.println("Enter the Branch\n");
+			s[i].branch = sc.next();
+			System.out.println("Enter the Phone Number\n");
+			s[i].phone = sc.nextLong();
 		}
-		for(int i=0;i<n;i++) {
-		System.out.println("USN"+"\t"+"Name"+"\t"+"Branch"+"\t"+"Phone number");
-		for(int j=0;j<n;j++) 
-		{
-			 s[j].display();
+		System.out.println("USN\t\tNAME\t\tBRANCH\t\tPHONE NO");
+		for(int i = 0; i < n ; i++) {
+			s[i].display();			
 		}
-		}
-		sc.close();
-	}  
+	}
+
 }
--- oopslab/src/program2/Student.java
+++ oopslab/src/program2/Student.java
@@ -4,11 +4,10 @@ public class Student {
 	
 	
 	
-	
-	String usn ,name, branch ;
-	long phoneno ;
-	void display() {
-		System.out.println(usn+"\t"+name+"\t"+branch+"\t"+phoneno);
+	String usn, name, branch;
+	long phone;
+	void display(){
+		System.out.println(usn + "\t\t" + name + "\t\t" + branch + "\t\t" + phone);
 	}
 
 }
