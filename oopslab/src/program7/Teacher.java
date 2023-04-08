6f873aceb46b75cafece5e34db2f5da1e972be6b
vinayak- thakur <vinayak.cmrit@gmail.com>
2023-04-08 20:38:58 +0530

    commit

--- oopslab/src/program7/Teacher.java
+++ oopslab/src/program7/Teacher.java
@@ -1,27 +1,30 @@
 package program7;
+
 import java.util.Scanner;
+
 public class Teacher implements Resume {
-	String name,ach,qua;
-	int age,exp;
+	String name, ach, qua;
+	int age, exp;
+
 	@Override
 	public void BioData() {
 		Scanner sc = new Scanner(System.in).useDelimiter("\n");
 		System.out.println("Enter the Name of the teacher:");
-		name=sc.next();
+		name = sc.next();
 		System.out.println("Enter the achievements of the teacher:");
-		ach=sc.next();
+		ach = sc.next();
 		System.out.println("Enter the qualification of the teacher:");
-		qua=sc.next();
+		qua = sc.next();
 		System.out.println("Enter the age of the teacher:");
-		age=sc.nextInt();
+		age = sc.nextInt();
 		System.out.println("Enter the experience of the teacher:");
-		exp=sc.nextInt();
-		
+		exp = sc.nextInt();
+
 		System.out.println("----------RESUME-------------");
-		System.out.println("NAME             :"+name);
-		System.out.println("AGE              :"+age);
-		System.out.println("QUALIFICATION    :"+qua);
-		System.out.println("EXPERIENCE       :"+exp);
-		System.out.println("ACHIEVEMENTS     :"+ach);
+		System.out.println("NAME             :" + name);
+		System.out.println("AGE              :" + age);
+		System.out.println("QUALIFICATION    :" + qua);
+		System.out.println("EXPERIENCE       :" + exp);
+		System.out.println("ACHIEVEMENTS     :" + ach);
 	}
 }
